82feab76485fafdce019e7db04488239a5321d01
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2011-11-17 01:09:02 -0800

    Fixed several bugs and added useful scopes and support for active and inactive importance indicators.

--- lib/acts_as_important/acts_as_important.rb
+++ lib/acts_as_important/acts_as_important.rb
@@ -2,20 +2,38 @@ module ActsAsImportant
   module ActMethod #:nodoc:
     def acts_as_important(options = {})
       has_many :importance_indicators, :as => :record, :dependent => :destroy
-      
-      scope :important_to, lambda{|user| joins(:importance_indicators).where(:importance_indicators => {:user_id => user.id})}
-      scope :not_important_to, lambda{|user| joins("LEFT OUTER JOIN importance_indicators ON importance_indicators.record_id = #{quoted_table_name}.id AND importance_indicators.record_type = '#{name}' AND importance_indicators.user_id = #{user.id}").where("importance_indicators.record_id IS NULL")}
 
-      extend ActsAsSubscribable::ClassMethods
-      include ActsAsSubscribable::InstanceMethods
+      # Left joins importance indicators from a particular user
+      scope :with_user_importance, lambda{|user| joins("LEFT OUTER JOIN importance_indicators ON importance_indicators.record_id = #{quoted_table_name}.id AND importance_indicators.record_type = '#{name}' AND importance_indicators.user_id = #{user.id}")}
+      
+      scope :important_to, lambda{|user| joins(:importance_indicators).where("importance_indicators.user_id = #{user.id} AND importance_indicators.active = true")}
+      scope :was_important_to, lambda{|user| joins(:importance_indicators).where("importance_indicators.user_id = #{user.id} AND importance_indicators.active = false")}
+      scope :not_important_to, lambda{|user| with_user_importance(user).where("importance_indicators.record_id IS NULL")}
+      scope :important_to_user_first, lambda{|user| with_user_importance(user).reorder('importance_indicators.record_id IS NOT NULL DESC')}
+
+      extend ActsAsImportant::ClassMethods
+      include ActsAsImportant::InstanceMethods
+    end
+    
+    # Call this method from the user model
+    def concered_with_importance
+      has_many :importance_indicators
     end
   end
 
   module ClassMethods
-  end
+    # Find all the importance_indicators of the records by the user in a single SQL query and cache them in the records for use in the view.
+    def cache_importance_for(records, user)
+      importance_indicators = ImportanceIndicator.where(:record_type => name, :record_id => records.collect(&:id), :user_id => user.id)
 
+      for record in records
+        record.cached_importance = importance_indicators.detect {|importance_indicator| importance_indicator.record_id == record.id} || false
+      end
+    end
+  end
+  
   module InstanceMethods
-    attr_accessor :cached_subscription
+    attr_accessor :cached_importance
 
     def acts_like_important?
       true
@@ -28,7 +46,30 @@ module ActsAsImportant
     end
     
     def important_to?(user)
-      importance_indicators.where(:user_id => user.id).exists?
+      if indicator = importance_indicator_for(user)
+        indicator.active?
+      end
+    end
+
+    def was_important_to?(user)
+      if indicator = importance_indicator_for(user)
+        !indicator.active?
+      end
+    end
+
+    def importance_indicator_for(user)
+      case cached_importance
+      when nil
+        importance_indicators.find_by_user_id(user.id)
+      when false
+        nil
+      else
+        cached_importance
+      end
+    end
+    
+    def importance_note_for(user)
+      importance_indicator_for(user).try(:note)
     end
   end
 end
